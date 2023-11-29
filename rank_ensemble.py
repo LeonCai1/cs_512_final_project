8d9a6dfcdcbac17c232e6ab28e147a75c7c1dfbd
yueyis2 <55012147+yueyis2@users.noreply.github.com>
2023-11-29 17:45:09 -0600

    Add files via upload
    rank_ensemble_1.0

---
+++ rank_ensemble.py
@@ -0,0 +1,102 @@
+import json
+from collections import defaultdict
+import math
+from nltk.corpus import stopwords
+import numpy as np
+import argparse
+from utils import *
+
+
+from nltk.corpus import wordnet
+def get_similarity(word1, word2):
+    similarity = None
+    # Get synsets for the word
+    synsets1 = wordnet.synsets(word1)
+    synsets2 = wordnet.synsets(word2)
+
+    temp = []
+    for synset1 in synsets1:
+        for synset2 in synsets2:
+            if synset1 and synset2:
+                temp.append(synset1.path_similarity(synset2))
+        
+    similarity = max(temp)
+    return similarity
+
+def rank_ensemble(args, topk=20):
+
+    ## embedding got from CatE
+    word2emb = load_cate_emb(f'datasets/{args.dataset}/emb_{args.topic}_w.txt')
+    ## embedding got from bert(PLM)
+    word2bert = load_bert_emb(f'datasets/{args.dataset}/{args.dataset}_bert')
+
+    caseolap_results = []
+    with open(f'datasets/{args.dataset}/intermediate_2.txt') as fin:
+        for line in fin:
+            data = line.strip()
+            _, res = data.split(':')
+            caseolap_results.append(res.split(','))
+            
+    cur_seeds = []
+    with open(f'datasets/{args.dataset}/{args.topic}_seeds.txt') as fin:
+        for line in fin:
+            data = line.strip().split(' ')
+            cur_seeds.append(data)
+
+
+    with open(f'datasets/{args.dataset}/{args.topic}_seeds.txt', 'w') as fout:
+        for seeds, caseolap_res in zip(cur_seeds, caseolap_results):
+            word2mrr = defaultdict(float)
+
+            # cate mrr
+            word2cate_score = {word:np.mean([np.dot(word2emb[word], word2emb[s]) for s in seeds]) for word in word2emb}
+            r = 1. ## r suppose to means rank
+            for w in sorted(word2cate_score.keys(), key=lambda x: word2cate_score[x], reverse=True)[:topk]:
+                if w not in word2bert: continue
+                word2mrr[w] += 1./r
+                r += 1
+                 
+            # bert mrr
+            word2bert_score = {word:np.mean([np.dot(word2bert[word], word2bert[s]) for s in seeds]) for word in word2bert}
+            r = 1.
+            for w in sorted(word2bert_score.keys(), key=lambda x: word2bert_score[x], reverse=True)[:topk]:
+                if w not in word2emb: continue
+                word2mrr[w] += 1./r
+                r += 1
+            
+            # caseolap mrr
+            r = 1.
+            for w in caseolap_res[:topk]:
+                word2mrr[w] += 1./r
+                r += 1
+            
+
+            def wordnet_score(word2mrr):
+                r = 1.
+                similarity = 0.
+                for w in word2mrr.keys():
+                    for s in seeds:
+                        temp2 = []
+                        temp2.append(get_similarity(w, s))                    
+                    similarity = max(temp2)
+                word2mrr[w]  += similarity
+                r += 1
+
+            wordnet_score(word2mrr)   
+            ## word2mrr 在这里包含了三种办法的平均rank 如果能用这个平均rank去和seed的关系做判断再去修改这个rank会准确（prompt-based）（有点难）
+            ## word2mrr 如果给rank再加上一个wordnet的score也许会更准确
+            ## word2mrr 这个rank也相当于一种normalized过后的score
+            score_sorted = sorted(word2mrr.items(), key=lambda x: x[1], reverse=True)  ##从大到小sort
+            top_terms = [x[0].replace(' ', '') for x in score_sorted if x[1] > args.rank_ens and x[0] != '']
+            fout.write(' '.join(top_terms) + '\n')
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='main', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--dataset', default='nyt', type=str)
+    parser.add_argument('--topic', default='topic', type=str)
+    parser.add_argument('--topk', default=20, type=int)
+    parser.add_argument('--rank_ens', default=0.3, type=float)
+    args = parser.parse_args()
+
+    rank_ensemble(args, args.topk)
